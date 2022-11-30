b8b859cf4b86bf1b0eb3f0e4766c7f35d1cef7a3
Amogh Chaubey <chaubeyamogh@gmail.com>
2022-11-30 18:15:00 -0500

    Scraped the news from FinViz

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 from urllib.request import urlopen, Request
-from pip import bs4
+from bs4 import BeautifulSoup
+
+
+
+finviz_url = 'https://finviz.com/quote.ashx?t='
+tickers = ['AMZN', 'AMD', 'FB', 'CRBP', 'SATX', 'XPEV', 'DOW', 'AVDL']
+
+
+news_tables = {}
+for ticker in tickers: 
+    url = finviz_url + ticker
+
+    req = Request(url=url, headers={'user-agent': 'my-app'})
+    response = urlopen(req)
+    print(response)
+    
+
+    html = BeautifulSoup(response, 'html')
+    news_table = html.find(id='news-table')
+    news_tables[ticker] = news_table
+    break
+
+print(news_table)
