4420aae9c8a9a1336bddff70d90fddc768a959f1
Amogh Chaubey <chaubeyamogh@gmail.com>
2022-11-30 18:56:24 -0500

    this shit isn't working man

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from urllib.request import urlopen, Request
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as BS
 
 
 
@@ -13,12 +13,30 @@ for ticker in tickers:
 
     req = Request(url=url, headers={'user-agent': 'my-app'})
     response = urlopen(req)
-    print(response)
+ 
     
 
-    html = BeautifulSoup(response, 'html')
+    html = BS(response, 'html')
     news_table = html.find(id='news-table')
     news_tables[ticker] = news_table
     break
 
-print(news_table)
+
+
+parsed_data = []
+for ticker, news_table in news_tables.items():
+    for row in news_table.findAll('tr'):
+        title = row.a.text
+        date_data = row.td.text.split(' ')
+
+        if len(date_data) == 1:
+            time = date_data[0]
+        else:
+            date = date_data[0]
+            time = date_data[1]
+
+        parsed_data.append([ticker, date, time, title])
+
+print(parsed_data)
+    
+
